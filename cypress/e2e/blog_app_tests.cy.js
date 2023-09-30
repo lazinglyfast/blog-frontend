6eaa58efab626a37f0224449cacc9d13af03288f
diogofriggo <diogofriggobr@gmail.com>
2023-09-30 19:24:21 +0200

    ex5.19

--- cypress/e2e/blog_app_tests.cy.js
+++ cypress/e2e/blog_app_tests.cy.js
@@ -9,4 +9,34 @@ describe("Blog app", () => {
     cy.contains("password").get("input")
     cy.contains("login").get("button")
   })
+
+  describe("with an existing user", () => {
+    beforeEach(() => {
+      const user = {
+        name: "oswald",
+        username: "coala",
+        password: "bear",
+      }
+      cy.request({
+        method: "POST",
+        url: `${Cypress.env("backendUrl")}/api/users`,
+        body: user,
+      })
+    })
+
+    it("User can successfully login with correct credentials", () => {
+      cy.get("#username").type("coala")
+      cy.get("#password").type("bear")
+      cy.contains("login").click()
+      cy.contains("coala logged in")
+    })
+
+    it("User login fails with incorrect credentials", () => {
+      cy.get("#username").type("coala")
+      cy.get("#password").type("this is not my password")
+      cy.contains("login").click()
+      cy.contains("Invalid username and/or password")
+        .should("have.css", "color", "rgb(255, 0, 0)")
+    })
+  })
 })
